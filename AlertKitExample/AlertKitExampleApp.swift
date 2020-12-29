adb676265b8997aefb2d6d20077cc89d28b17862
Alex Nagy <therebeloper@gmail.com>
2020-12-29 13:24:03 +0200

    Initial Commit

---
+++ AlertKitExample/AlertKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertKitExampleApp.swift
+//  AlertKitExample
+//
+//  Created by Alex Nagy on 29.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct AlertKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
